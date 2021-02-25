1b95e771ce0554eefacd6056b81b426268b894dd
HaakonBM <wolfnr1@hotmail.com>
2021-02-25 22:46:13 +0100

    First
    All intro parts

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+
+
+
+
+    void OnCollisionEnter(Collision colInfo)
+    {
+       
+        string colidedName = colInfo.collider.tag;
+        bool colidedWithObstacle = colidedName.StartsWith("Obstacle");
+
+        if (colidedWithObstacle)
+        {
+            movement.enabled = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody playerBody;
+    public float forwardForce = 500f;
+    public float sidewaysForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //playerBody.AddForce(0, 0, forwardForce * Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            //playerBody.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            playerBody.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(Input.GetKey("a"))
+        {
+            playerBody.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+   
+        }
+        if(Input.GetKey("w"))
+        {
+            playerBody.AddForce(0,0, forwardForce);
+        }
+    }
+}
