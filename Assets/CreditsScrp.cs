226455237f9abd0d98f78d7ae197832f1130658d
HaakonBM <wolfnr1@hotmail.com>
2021-03-06 23:57:17 +0100

    MainMenu and Ending added

---
+++ Assets/CreditsScrp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsScrp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnApplicationQuit()
+    {
+        
+    }
+    public void Quit ()
+    {
+        Debug.Log("Quitting");
+        Application.Quit(); 
+    }
+
+}
