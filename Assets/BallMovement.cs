5b770253153b17952581aef704ed67b89c4fff40
HaakonBM <wolfnr1@hotmail.com>
2021-03-11 22:52:02 +0100

    Movement updated

--- Assets/BallMovement.cs
+++ Assets/BallMovement.cs
@@ -8,6 +8,8 @@ public class BallMovement : MonoBehaviour
     public Rigidbody ballBody;
     private float forwardV = 50f;
     private float sidewaysV = 50f;
+
+    public int moves = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -46,6 +48,29 @@ public class BallMovement : MonoBehaviour
         if (collTileBorder)
         {
             Debug.Log("Went through TILEBORDER");
+            if(moveVal())
+            {
+                move();
+                Debug.Log("You have " + moves + " left");
+
+            } else
+            {
+                Debug.Log("You have " + moves + " left, and are out of moves");
+            }
         }
+    }
+
+    private void move ()
+    {
+        this.moves -= 1;
+    }
+
+    private bool moveVal()
+    {
+        if(moves > 0)
+        {
+            return true;
+        }
+        return false;
     }
 }
