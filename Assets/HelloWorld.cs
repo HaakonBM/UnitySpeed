6bb861d2765df186d76996c18ec62fb4866523e8
HaakonBM <wolfnr1@hotmail.com>
2021-03-18 20:56:11 +0100

    NeonStart

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HelloWorld : MonoBehaviour
+{
+    [SerializeField] private int number;
+
+    private void Start()
+    {
+        Debug.Log("Hello World"); 
+    }
+}
